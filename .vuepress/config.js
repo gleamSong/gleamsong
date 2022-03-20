94ecbf7ef9f614ad87f59073aa59bd3e1f48f440
songjg <toblessong@163.com>
2022-03-21 00:50:46 +0800

    初始化提交

---
+++ .vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: 'THOUGHT & NEW VIEW',
+  description: '记录一些想法，以及其他值得记录的东西',
+  plugins:['@vuepress-reco/vuepress-plugin-rss',{site_url:'https://blog.gleams.cyou'}],
+  
+}
